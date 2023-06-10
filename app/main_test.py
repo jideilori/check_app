76b30d605c632ae0b7cc08395e5912bd421a1afb
Oluwaseun Ilori <jideilori77@gmail.com>
2023-06-10 15:39:57 +0100

    modified test

--- app/main_test.py
+++ app/main_test.py
@@ -4,7 +4,7 @@ import os
 
 class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
-        random_string = "This is my tes t string"
+        random_string = "This is my test string"
         random_string_reversed ="gnirts tset ym si sihT"
 
         self.assertEqual(random_string_reversed,
